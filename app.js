e46e373b61885ae0a57fbad525d5594eac368194
SamThompson57 <s_thomo@hotmail.co.uk>
2024-09-14 08:11:27 +0100

    Added the necessary files for fly.io hosting

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // app file
 const express = require('express');
+const createError = require('http-errors')
 const userRouter = require('./routes/index.js');
-const path = require("node:path");
+const path = require("path");
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 const app = express();
 
 const indexRouter = require('./routes/index.js');
@@ -10,6 +13,9 @@ const msgRouter = require('./routes/message.js')
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser())
 
 
 app.set("views", path.join(__dirname, "views"));
@@ -24,5 +30,23 @@ app.use('/', indexRouter);
 app.use('/new', newRouter);
 app.use('/msgdetail', msgRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`I am listening on port ${PORT}!`));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+//const PORT = 3000;
+//app.listen(PORT, () => console.log(`I am listening on port ${PORT}!`));
+
+module.exports = app
